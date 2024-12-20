a8cee1cf59b8b3f847646aa92714b731f934d5e0
wanghai <wanghai@qilitech.ltd>
2024-12-20 17:51:35 +0800

    update project gradle version

--- sample/src/main/java/com/orhanobut/sample/MainActivity.kt
+++ sample/src/main/java/com/orhanobut/sample/MainActivity.kt
@@ -12,14 +12,18 @@ class MainActivity : Activity() {
     setContentView(R.layout.activity_main)
 
     LogKit.initAllLog(this)
-    LogKit.v("A","hello %s", "world")
-    LogKit.d("b","输出调试信息 debug my log message")
-    LogKit.i("C","输出必要信息 info my log message")
-    LogKit.w("d","输出警告信息 warn my log message")
-    LogKit.e("E","输出错误信息 error my log message")
-    findViewById<View>(R.id.tv_view).postDelayed({
+    for (i in 0..500){
+      LogKit.v("A","hello %s", "world")
+      LogKit.d("b","输出调试信息 debug my log message")
+      LogKit.i("C","输出必要信息 info my log message")
+      LogKit.w("d","输出警告信息 warn my log message")
+      LogKit.e("E","输出错误信息 error my log message")
+    }
+
+    findViewById<View>(R.id.tv_view).post({
       LogKit.showLogUi(this)
-    }, 2000)
+      finish()
+    })
 
   }
 
